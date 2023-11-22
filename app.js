7078c4f1f960db732894e5bbe42f4284cd9f742a
jgadzinska <joanna.gadzinska88@wp.pl>
2023-11-22 09:33:54 +0100

    New app.js

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const body = document.querySelector("body");
 const nav = document.querySelector("nav");
 const searchToggle = document.querySelector(".searchToggle");
-const navOpenBtn = document.querySelector(".navOpenBtn");
-const navCloseBtn = document.querySelector(".navCloseBtn");
+
 
 
 
 searchToggle.addEventListener("click", () => {
     searchToggle.classList.toggle("active");
-});
-
-navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-});
-navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
 });
